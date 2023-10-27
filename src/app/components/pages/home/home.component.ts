38f7515398ee3a055f4ad75c4749f48b8a919647
leopoldocalado <leopoldocalado@outlook.com>
2023-10-27 08:17:13 -0700

    Criado modal

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  closeResult!: string;
+
+	constructor(private modalService: NgbModal) {}
+
+	openBackDropCustomClass(content: any) {
+		this.modalService.open(content, { backdropClass: 'light-blue-backdrop' });
+	}
+
+	openWindowCustomClass(content: any) {
+		this.modalService.open(content, { windowClass: 'dark-modal' });
+	}
+
+	openLg(content: any) {
+		this.modalService.open(content, { size: 'lg' });
+	}
+
+
 
 }
